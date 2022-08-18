c92b1b395c3e8698941fb6adef584ef2446cd180
EthanKettle <97110734+EthanKettle@users.noreply.github.com>
2022-08-18 07:44:21 -0600

    hey

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 
 class App extends React.Component{
+  state = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    email: '',
+    age: 0,
+
+  }
   render() {
     return (
       <div>
         <h2>Database App</h2>
         <form>
+          {/* <div class="form-input">
+            <input type="Number" name="id" placeholder="id" value="" onChange={id}/>
+          </div> */}
           <div class="form-input">
-            <input type="Number" name="id" value="" onChange={id}/>
+            <input type="text" name="firstName" placeholder="First Name" value="" onChange={firstName}/>
           </div>
           <div class="form-input">
-            <input type="text" name="firstName" value="" onChange={firstName}/>
+            <input type="text" name="lastName" placeholder="Last Name" value="" onChange={lastName}/>
           </div>
           <div class="form-input">
-            <input type="text" name="lastName" value="" onChange={lastName}/>
+            <input type="text" name="email" placeholder="Email@example" value="" onChange={email}/>
           </div>
           <div class="form-input">
-            <input type="text" name="email" value="" onChange={email}/>
-          </div>
-          <div class="form-input">
-            <input type="Number" name="age" value="" onChange={age}/>
+            <input type="Number" name="age" placeholder="0" value="" onChange={age}/>
           </div>
           <button>Submit Form</button>
         </form>
